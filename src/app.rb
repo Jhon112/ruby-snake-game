e1ddd46c542057405de797d59c64d76600640f59
Jhon Arias <jhonarmont@hotmail.com>
2019-11-16 03:28:00 -0500

    This is our entry point
    We start a view and from  timer we call our action and render the state

---
+++ src/app.rb
@@ -0,0 +1,37 @@
+require_relative "view/ruby2d"
+require_relative "models/state"
+require_relative "controller/actions"
+
+class App
+    def initialize
+        @state = initial_state = Models::initial_state # call initial_state method from Models
+        # This method creates a state instance who receives a snake, food, and grid objs
+        # To create those objects we instance each class as well
+        
+    end
+
+    def start
+        # Instance Ruby2dView
+        view = View::Ruby2dView.new
+
+        # start uses main thread to do the render, because of that we need a new thread
+        # to handle the render
+        Thread.new { init_timer(view) }
+
+        # start window
+        view.start(@state)
+    end
+
+    def init_timer(view)
+        loop do
+            # trigger movement
+            @state = Actions::move_snake(@state)
+            # We call method render from view instance
+            view.render(@state)
+            sleep 0.5
+        end
+    end
+end
+
+app = App.new
+app.start
