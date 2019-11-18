dc6857a2e46f095241b93f486ffab88ec781202c
Jhon Arias <jhonarmont@hotmail.com>
2019-11-18 01:58:45 -0500

    Reset instance with new values so the snake moves

--- src/view/ruby2d.rb
+++ src/view/ruby2d.rb
@@ -29,9 +29,11 @@ module View
         # iterate over snake's positions attribute and create a square for each
         # Note that positions is an array of Coord instances
         def render_snake(state)
+            @snake_positions.each(&:remove) if @snake_positions
+            
             extend Ruby2D::DSL
             snake = state.snake
-            snake.positions.each do |pos|
+            @snake_positions = snake.positions.map do |pos|
                 Square.new(
                     color: 'green',
                     x: pos.col * @pixel_size,
@@ -44,9 +46,10 @@ module View
         # assign food instance to food variable
         # create a square according to food attributes
         def render_food(state)
+            @food.remove if @food
             extend Ruby2D::DSL
             food = state.food
-            Square.new(
+            @food = Square.new(
                 color: 'white',
                 x: food.col * @pixel_size,
                 y: food.row * @pixel_size,
