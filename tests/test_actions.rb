3edb3450eb72a56e72cc2f4fc12dc6b9e28aaaee
Jhon Arias <jhonarmont@hotmail.com>
2019-11-18 02:58:58 -0500

    Implement test for snake movement

--- tests/test_actions.rb
+++ tests/test_actions.rb
@@ -4,8 +4,9 @@ require_relative "../src/models/state"
 
 
 class ActionsTest < Minitest::Test
-    def test_move_snake
-        initial_state = Models::State.new(
+    
+    def setup
+        @initial_state = Models::State.new(
             Models::Snake.new([
                 Models::Coord.new(1,1),
                 Models::Coord.new(0,1)
@@ -15,7 +16,9 @@ class ActionsTest < Minitest::Test
             Models::Direction::DOWN,
             false
         )
-
+    end
+    
+    def test_move_snake
         expected_state = Models::State.new(
             Models::Snake.new([
                 Models::Coord.new(2,1),
@@ -27,7 +30,32 @@ class ActionsTest < Minitest::Test
             false
         )
 
-        actual_state = Actions::move_snake(initial_state)
+        actual_state = Actions::move_snake(@initial_state)
+        assert_equal actual_state, expected_state
+    end
+
+    def test_change_direction_invalid
+        expected_state = @initial_state
+
+        # As snake is going dow, it can't go up
+        actual_state = Actions::change_direction(@initial_state, Models::Direction::UP)
+        assert_equal actual_state, expected_state
+    end
+
+    def test_change_direction_valid
+        expected_state = Models::State.new(
+            Models::Snake.new([
+                Models::Coord.new(1,1),
+                Models::Coord.new(0,1)
+            ]),
+            Models::Food.new(4,4),
+            Models::Grid.new(8,12),
+            Models::Direction::LEFT,
+            false
+        )
+
+        # As snake is going dow, it can't go up
+        actual_state = Actions::change_direction(@initial_state, Models::Direction::LEFT)
         assert_equal actual_state, expected_state
     end
 end
